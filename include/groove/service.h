e0c36857891285aff2a597e7053a7a127377b33f
Pippijn van Steenhoven <pip88nl@gmail.com>
2010-09-21 18:44:20 +0200

    some more stubs

--- include/groove/service.h
+++ include/groove/service.h
@@ -8,9 +8,37 @@
 #include "groove/private/make_varmap.h"
 class GrooveClient;
 
+#include <QDate>
 #include <QVariant>
 
 #include <memory>
+
+enum class TagID
+{
+  Rap = 3,
+  RnB = 4,
+  Rock = 12,
+  Alternative = 13,
+  Metal = 17,
+  HipHop = 29,
+  Jazz = 43,
+  Pop = 56,
+  Electronica = 67,
+  Trance = 69,
+  Ambient = 75,
+  Country = 80,
+  Bluegrass = 96,
+  Oldies = 102,
+  Punk = 111,
+  Folk = 122,
+  Indie = 136,
+  Reggae = 160,
+  Experimental = 191,
+  Blues = 230,
+  Latin = 528,
+  Classical = 750,
+  ClassicRock = 3529,
+};
 
 class LIBGROOVESHARED_EXPORT GrooveService
   : public QObject
@@ -21,6 +49,10 @@ private: // types
   typedef QVariantOrMap::pair pair;
   typedef QVariantOrMap::map map;
   typedef QVariantOrMap::array array;
+
+  typedef uint UserID;
+  typedef uint SongID;
+  typedef uint AlbumID;
 
 private: // functions
   pair country () const;
@@ -36,31 +68,31 @@ public: // functions
   void artistGetSimilarArtists (uint artistID);
   void artistGetSongs (uint offset, uint artistID, bool isVerified);
   void authenticateUserEx (QString password, QString username);
-  void autoplayGetSong (array frowns);
-  void autoplayRemoveVoteDownEx ();
-  void autoplayRemoveVoteUpEx ();
-  void autoplayVoteDownEx ();
-  void autoplayVoteUpEx ();
-  void broadcastSong ();
-  void createWidgetIDFromSongIDs ();
-  void deletePlaylist ();
-  void favorite ();
-  void forgotPassword ();
-  void getAffiliateDownloadURLs ();
+  void autoplayGetSong ();
+  void autoplayRemoveVoteDownEx (uint songQueueID, uint songQueueSongID);
+  void autoplayRemoveVoteUpEx (uint songQueueID, uint songQueueSongID);
+  void autoplayVoteDownEx (uint songQueueID, uint songQueueSongID);
+  void autoplayVoteUpEx (uint songQueueID, uint songQueueSongID);
+  void broadcastSong (QString message, QString link, QString username, QString password, uint songID, QString service, bool saveCredentials);
+  void createWidgetIDFromSongIDs (QList<uint> songIDs);
+  void deletePlaylist (uint playlistID);
+  void favorite (uint id, QString what);
+  void forgotPassword (void);
+  void getAffiliateDownloadURLs (QString songName, QString artistName);
   void getArtistAutocomplete ();
-  void getArtistByID ();
-  void getArtistsForTag ();
+  void getArtistByID (uint artistID);
+  void getArtistsForTag (TagID tagID);
   void getAutoCoolPlaylistName ();
-  void getCombinedFeedData ();
+  void getCombinedFeedData (uint loggedInUserID, QDate day, QList<uint> userIDs);
   void getCommunicationToken ();
   void getContactInfoForFollowedUsers ();
   void getCountryFromRequestIP ();
-  void getDetailsForBroadcast ();
+  void getDetailsForBroadcast (uint songID);
   void getEmailAddress ();
-  void getFavouriteIDs ();
-  void getFavourites ();
+  void getFavouriteIDs (uint userID, QString ofWhat);
+  void getFavourites (uint userID, QString ofWhat);
   void getPlaylistByID ();
-  void getQueueSongListFromSongIDs ();
+  void getQueueSongListFromSongIDs (QList<uint> songIDs);
   void getRecentlyActiveUsers ();
   void getSearchResults (QString searchTerm, QString type);
   void getSimilarUsers ();
